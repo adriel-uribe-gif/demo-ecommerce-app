1519daf63914557fb832f1cd514241419b9b10db
adriel-uribe-gif <008255202@coyote.csusb.edu>
2026-07-07 11:02:17 -0700

    Wire payments route into app

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const authRoutes = require('./src/auth');
 const productRoutes = require('./src/products');
 const orderRoutes = require('./src/orders');
+const paymentRoutes = require('./src/payments');
 
 const app = express();
 app.use(express.json());
@@ -9,6 +10,7 @@ app.use(express.json());
 app.use('/auth', authRoutes);
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/payments', paymentRoutes);
 
 app.listen(3000, () => {
     console.log('Server running on port 3000');
